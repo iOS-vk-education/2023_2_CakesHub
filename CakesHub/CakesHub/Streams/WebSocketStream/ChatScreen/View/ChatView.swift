f6d2cee8247100d4ebd0164375bb18c746a5966f
Dmitriy Permyakov <dimapermyakov55@gmail.com>
2024-03-27 19:23:13 +0300

    чат

--- CakesHub/CakesHub/Streams/WebSocketStream/ChatScreen/View/ChatView.swift
+++ CakesHub/CakesHub/Streams/WebSocketStream/ChatScreen/View/ChatView.swift
@@ -11,9 +11,9 @@ import SwiftUI
 struct ChatView: View, ViewModelable {
     typealias ViewModel = ChatViewModel
 
-    #warning("Удалите навигацию, если она не требуется")
     @EnvironmentObject private var nav: Navigation
     @StateObject var viewModel: ViewModel
+    @State private var messageText: String = .clear
 
     init(viewModel: ViewModel = ViewModel()) {
         self._viewModel = StateObject(wrappedValue: viewModel)
@@ -29,7 +29,6 @@ struct ChatView: View, ViewModelable {
 
 private extension ChatView {
 
-    #warning("Тут логика при появлении экрана")
     func onAppear() {
     }
 }
@@ -38,39 +37,151 @@ private extension ChatView {
 
 private extension ChatView {
 
-    #warning("Тут должна быть вашь вьюха")
     var MainView: some View {
-        VStack {
-            MKRImageView(
-                configuration: .basic(
-                    kind: viewModel.image,
-                    imageSize: CGSize(width: Constants.imageSize, height: Constants.imageSize),
-                    imageShape: .roundedRectangle(Constants.imageCornerRadius)
-                )
-            )
+        ScrollViewReader { proxy in
+            VStack(spacing: 0) {
+                ScrollView {
+                    LazyVStack {
+                        ForEach(viewModel.messages) { message in
+                            switch message.kind {
+                            case .bubble:
+                                MessageBubble(message: message)
+                                    .padding(.horizontal, 8)
 
-            Text(viewModel.title)
-                .style(14, .semibold, Constants.textColor)
+                            case .join:
+                                MessageJoinText(
+                                    userName: Constants.joinChatMessage(userName: message.userName)
+                                )
+
+                            case .quit:
+                                MessageJoinText(
+                                    userName: Constants.quitChatMessage(userName: message.userName)
+                                )
+                            }
+                        }
+                    }
+                    .padding(.bottom, 50)
+                }
+                .onTapGesture {
+                    UIApplication.shared.sendAction(
+                        #selector(UIResponder.resignFirstResponder),
+                        to: nil,
+                        from: nil,
+                        for: nil
+                    )
+                }
+
+                TextFieldBlock
+                    .padding(.vertical, 6)
+                    .background(Constants.bottomBackgroundColor)
+            }
+            .frame(maxHeight: .infinity)
+            .onChange(of: viewModel.lastMessageID) { _, id in
+                if let id {
+                    withAnimation {
+                        proxy.scrollTo(id, anchor: .bottom)
+                    }
+                }
+            }
+            .background {
+                BackgroundView
+                    .ignoresSafeArea()
+            }
         }
+        .onDisappear(perform: viewModel.quitChat)
+    }
+
+    var BackgroundView: some View {
+        LinearGradient(
+            colors: Constants.gradientBackgroundColor,
+            startPoint: .topLeading,
+            endPoint: .bottomTrailing
+        )
+        .mask {
+            Constants.tgBackground
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+    }
+
+    var TextFieldBlock: some View {
+        HStack {
+            Constants.paperClip
+
+            TextField(Constants.placeholder, text: $messageText)
+                .padding(.vertical, 6)
+                .padding(.horizontal, 13)
+                .background(Constants.textFieldBackgroundColor, in: .rect(cornerRadius: 20))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(lineWidth: 1)
+                        .fill(Constants.textFieldStrokeColor)
+                }
+
+            if messageText.isEmpty {
+                Constants.record
+                    .frame(width: 22, height: 22)
+
+            } else {
+                Button {
+                    viewModel.sendMessage(message: messageText)
+                    messageText = .clear
+                } label: {
+                    Image(systemName: Constants.paperplane)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 22, height: 22)
+                        .foregroundStyle(Constants.iconColor)
+                }
+            }
+        }
+        .padding(.horizontal, 8)
+    }
+
+    func MessageJoinText(userName: String) -> some View {
+        Text(userName)
+            .font(.caption)
+            .padding(.horizontal)
+            .padding(.vertical, 6)
+            .background(.black.opacity(0.7), in: .capsule)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
     }
 }
 
 // MARK: - Preview
 
-#warning("Удалите навигацию, если она не требуется")
 #Preview {
-    ChatView(viewModel: .mockData)
+    return ChatView(viewModel: .mockData)
         .environmentObject(Navigation())
 }
 
 // MARK: - Constants
 
-#warning("Добавляйте сюда все необходимые константы")
 private extension ChatView {
 
     enum Constants {
         static let imageSize: CGFloat = 200
         static let imageCornerRadius: CGFloat = 20
         static let textColor: Color = CHMColor<TextPalette>.textPrimary.color
+        static let tgBackground = Image("tg_layer")
+        static let paperClip = Image("paperClip")
+        static let record = Image("record")
+        static let messageBackgroundColor = Color(red: 103/255, green: 77/255, blue: 122/255)
+        static let textFieldBackgroundColor = Color(red: 6/255, green: 6/255, blue: 6/255)
+        static let textFieldStrokeColor = Color(red: 58/255, green: 58/255, blue: 60/255)
+        static let bottomBackgroundColor = Color(red: 28/255, green: 28/255, blue: 29/255)
+        static let iconColor = Color(red: 127/255, green: 127/255, blue: 127/255)
+        static let placeholder = "Message"
+        static let paperplane = "paperplane"
+        static func joinChatMessage(userName: String) -> String { "\(userName) вступил в чат" }
+        static func quitChatMessage(userName: String) -> String { "\(userName) покинул чат" }
+        static let gradientBackgroundColor: [Color] = [
+            Color(red: 168/255, green: 255/255, blue: 59/255),
+            Color(red: 111/255, green: 135/255, blue: 255/255),
+            Color(red: 215/255, green: 161/255, blue: 255/255),
+            Color(red: 113/255, green: 190/255, blue: 255/255),
+        ]
     }
 }
--- CakesHub/CakesHub/Streams/WebSocketStream/ChatScreen/View/Subviews/MessageBubble.swift
+++ CakesHub/CakesHub/Streams/WebSocketStream/ChatScreen/View/Subviews/MessageBubble.swift
@@ -5,4 +5,141 @@
 //  Created by Dmitriy Permyakov on 26.03.2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct MessageBubble: View {
+    let message: ChatMessage
+    var isYou: Bool { message.isYou }
+
+    var body: some View {
+        DefaultMessage
+    }
+}
+
+// MARK: - Subviews
+
+private extension MessageBubble {
+
+    var DefaultMessage: some View {
+        HStack(alignment: .bottom) {
+            if !isYou {
+                PersoneAvatar
+            }
+
+            VStack(alignment: isYou ? .trailing : .leading) {
+                VStack(alignment: .leading, spacing: 2) {
+                    if !isYou {
+                        Text(message.userName)
+                            .font(.system(size: 12, weight: .semibold))
+                            .foregroundStyle(.mint.gradient)
+                    }
+
+                    HStack(alignment: .bottom) {
+                        Text(message.message)
+
+                        HStack(spacing: 3) {
+                            Text(message.time)
+                                .foregroundStyle(.white.opacity(0.63))
+                                .font(.system(size: 11, weight: .semibold))
+
+                            if isYou {
+                                message.state.image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 14, height: 14)
+                                    .padding(.trailing, 5)
+                                    .foregroundStyle(message.state.imageColor)
+                            }
+                        }
+                    }
+                    .padding(.bottom, 4)
+                }
+                .padding(.leading, 10)
+                .padding([.top], 6)
+                .padding(.trailing, isYou ? 2 : 8)
+                .background(Constants.messageBackgroundColor, in: .rect(cornerRadius: 18))
+                .frame(maxWidth: 320, alignment: isYou ? .trailing : .leading)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: isYou ? .trailing : .leading)
+    }
+
+    var PersoneAvatar: some View {
+        Circle()
+            .fill(.mint.gradient)
+            .frame(width: 32, height: 32)
+            .overlay {
+                Text("\(message.userName.first?.description.uppercased() ?? "😎")")
+                    .foregroundStyle(.white)
+                    .fontWeight(.semibold)
+                    .fontDesign(.rounded)
+            }
+    }
+}
+
+// MARK: - Message State
+
+private extension Message.State {
+
+    var image: Image {
+        switch self {
+        case .error: return Constants.errorImage
+        case .received: return Constants.receivedImage
+        case .progress: return Constants.progressImage
+        }
+    }
+
+    var imageColor: Color {
+        switch self {
+        case .error: return Color(red: 1, green: 0, blue: 0)
+        default: return .white.opacity(0.63)
+        }
+    }
+}
+
+// MARK: - Preview
+
+#Preview {
+    VStack {
+        MessageBubble(
+            message: .init(
+                isYou: true,
+                message: "Hi! 🤗 You can switch patterns and gradients in the settings.",
+                userName: "mightyK1ngRichard",
+                time: "10:10",
+                state: .received
+            )
+        )
+
+        MessageBubble(
+            message: .init(
+                isYou: false,
+                message: "Воу рил неплохо",
+                userName: "Пермяков Дмитрий",
+                time: "10:10",
+                state: .error
+            )
+        )
+    }
+}
+
+// MARK: - Constants
+
+private extension Message.State {
+
+    enum Constants {
+        static let receivedImage = Image("checkMark2")
+        static let errorImage = Image(systemName: "exclamationmark.arrow.circlepath")
+        static let progressImage = Image(systemName: "clock.arrow.circlepath")
+    }
+}
+
+private extension MessageBubble {
+
+    enum Constants {
+        static let messageBackgroundColor = Color(red: 103/255, green: 77/255, blue: 122/255)
+        static let textFieldBackgroundColor = Color(red: 6/255, green: 6/255, blue: 6/255)
+        static let textFieldStrokeColor = Color(red: 58/255, green: 58/255, blue: 60/255)
+        static let bottomBackgroundColor = Color(red: 28/255, green: 28/255, blue: 29/255)
+    }
+}
