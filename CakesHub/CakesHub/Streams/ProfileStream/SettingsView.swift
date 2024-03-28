fa94c60b0c7177d0aabbc0311c9b88b7ddf79675
Dmitriy Permyakov <dimapermyakov55@gmail.com>
2024-03-28 16:28:44 +0300

    iOS-2: Подтянул iOS-4, добавил путь открытия настроек
    iOS-2: Убрал лишний NavigationPath

--- CakesHub/CakesHub/Streams/ProfileStream/SettingsView.swift
+++ CakesHub/CakesHub/Streams/ProfileStream/SettingsView.swift
@@ -7,61 +7,56 @@
 
 import SwiftUI
 
-struct SettingssView: View {
+struct SettingsView: View {
     var body: some View {
-            List {
-                Section(header: Text("Персональные данные")) {
-                    NavigationLink(destination: Text("Введите новый пароль")) {
-                        Label("Пароль", systemImage: "lock").foregroundColor(Constants.textColor)
-                    }
-                    NavigationLink(destination: Text("Введите новую почту")) {
-                        Label("Почта", systemImage: "envelope").foregroundColor(Constants.textColor)
-                    }
-                    Button(action: {}) {
-                        Label("Удалить аккаунт", systemImage: "trash")
-                    }
+        List {
+            Section(header: Text("Персональные данные")) {
+                NavigationLink(destination: Text("Введите новый пароль")) {
+                    Label("Пароль", systemImage: "lock").foregroundColor(Constants.textColor)
                 }
-                
-                Section(header: Text("Уведомления")) {
-                    NavigationLink(destination: NotificationsView()) {
-                        Label("Уведомления", systemImage: "bell").foregroundColor(Constants.textColor)
-                    }
+                NavigationLink(destination: Text("Введите новую почту")) {
+                    Label("Почта", systemImage: "envelope").foregroundColor(Constants.textColor)
                 }
-                
-                Section(header: Text("Документы")) {
-                    NavigationLink(destination: Text("Политика конфиденциальности")) {
-                        Label("Политика конфиденциальности", systemImage: "doc.text").foregroundColor(Constants.textColor)
-                    }
-                    NavigationLink(destination: Text("Пользовательское соглашение")) {
-                        Label("Пользовательское соглашение", systemImage: "doc.text").foregroundColor(Constants.textColor)
-                    }
-                }
-                
                 Button(action: {}) {
-                    Label("Support", systemImage: "questionmark.circle")
+                    Label("Удалить аккаунт", systemImage: "trash")
                 }
-                .padding(10)
             }
-            .listStyle(GroupedListStyle())
-            .navigationTitle("Настройки")
-            .background(Constants.bgColor)
+
+            Section(header: Text("Уведомления")) {
+                NavigationLink(destination: NotificationsView()) {
+                    Label("Уведомления", systemImage: "bell").foregroundColor(Constants.textColor)
+                }
+            }
+
+            Section(header: Text("Документы")) {
+                NavigationLink(destination: Text("Политика конфиденциальности")) {
+                    Label("Политика конфиденциальности", systemImage: "doc.text").foregroundColor(Constants.textColor)
+                }
+                NavigationLink(destination: Text("Пользовательское соглашение")) {
+                    Label("Пользовательское соглашение", systemImage: "doc.text").foregroundColor(Constants.textColor)
+                }
+            }
+
+            Button(action: {}) {
+                Label("Support", systemImage: "questionmark.circle")
+            }
+            .padding(10)
+        }
+        .listStyle(GroupedListStyle())
+        .navigationTitle("Настройки")
+        .background(Constants.bgColor)
     }
 }
-
-struct SettingsView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingssView()
-    }
-}
-
 
 #Preview {
-    SettingssView()
+    NavigationStack {
+        SettingsView()
+    }
 }
 
 // MARK: - Constants
 
-private extension SettingssView {
+private extension SettingsView {
     
     enum Constants {
         static let textColor = CHMColor<TextPalette>.textPrimary.color
--- CakesHub/CakesHub/Streams/ProfileStream/View/ProfileScreen.swift
+++ CakesHub/CakesHub/Streams/ProfileStream/View/ProfileScreen.swift
@@ -18,28 +18,41 @@ struct ProfileScreen: View {
     }
     
     var body: some View {
-        NavigationStack(path: $nav.path) {
-            MainView
-                .navigationDestination(for: ViewModel.Screens.self) { screen in
-                    switch screen {
-                    case .message:
-                        fatalError("Экран ещё не создан")
-                    case .notifications:
-                        fatalError("Экран ещё не создан")
-                    case .settings:
-                        fatalError("Экран ещё не создан")
-                    }
+        MainView
+            .navigationDestination(for: ViewModel.Screens.self) { screen in
+                switch screen {
+                case .message:
+                    Text("Экран чата")
+                case .notifications:
+                    Text("Экран уведомлений")
+                case .settings:
+                    SettingsView()
                 }
-        }
+            }
     }
 }
+
+// MARK: - Actions
 
 private extension ProfileScreen {
 
+    /// Нажатие на кнопку открытия чата
     func didTapOpenMessageScreen() {
         nav.addScreen(screen: ViewModel.Screens.message)
     }
+
+    /// Нажатие на кнопку открытия настроек
+    func didTapOpenSettings() {
+        nav.addScreen(screen: ViewModel.Screens.settings)
+    }
+    
+    /// Нажатие на кнопку открытия уведомлений
+    func didTapOpenNotifications() {
+        nav.addScreen(screen: ViewModel.Screens.notifications)
+    }
 }
+
+// MARK: UI Components
 
 private extension ProfileScreen {
 
@@ -52,7 +65,6 @@ private extension ProfileScreen {
                     let minY = geo.frame(in: .global).minY
                     HStack {
                         Button {
-                            Logger.log(message: "Нажатие на кнопку `message`")
                             didTapOpenMessageScreen()
                         } label: {
                             Label("message", systemImage: "message")
@@ -63,8 +75,8 @@ private extension ProfileScreen {
                                 .frame(width: 240, height: 45)
                                 .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30))
                         }
-                        Cbutton(iconname: .bell, action: {})
-                        Cbutton(iconname: .bell, action: {})
+                        Cbutton(iconname: UIImage(systemName: "gear"), action: didTapOpenSettings)
+                        Cbutton(iconname: .bell, action: didTapOpenNotifications)
                     }
                     .frame(maxWidth: .infinity)
                     .offset(y: max(60 - minY, 0))
@@ -171,13 +183,16 @@ fileprivate struct ProductFeedView: View {
 }
 
 fileprivate struct Cbutton: View {
-    let iconname: UIImage
+    let iconname: UIImage?
     var action: () -> Void
     var body: some View {
         Button {
             action()
         } label: {
-            Image(uiImage: iconname).renderingMode(.template).resizable().scaledToFill()
+            Image(uiImage: iconname ?? UIImage())
+                .renderingMode(.template)
+                .resizable()
+                .scaledToFill()
                 .foregroundStyle(CHMColor<IconPalette>.iconSecondary.color)
                 .frame(width: 23, height: 23)
                 .padding(10)
@@ -185,8 +200,6 @@ fileprivate struct Cbutton: View {
         }
     }
 }
-
-
 
 // MARK: - Constants
 
@@ -207,5 +220,3 @@ private extension ProfileScreen {
     }
     .environmentObject(Navigation())
 }
-
-
