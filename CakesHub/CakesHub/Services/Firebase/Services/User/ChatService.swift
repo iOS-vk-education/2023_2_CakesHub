cde489fd56721139880614e335b5c839b06583f2
Dmitriy Permyakov <dimapermyakov55@gmail.com>
2024-05-08 02:13:55 +0300

    iOS-24: Сверстал компонент ячейки чата

---
+++ CakesHub/CakesHub/Services/Firebase/Services/User/ChatService.swift
@@ -0,0 +1,10 @@
+//
+//
+//  ChatService.swift
+//  CakesHub
+//
+//  Created by Dmitriy Permyakov on 08.05.2024.
+//  Copyright 2024 © VK Team CakesHub. All rights reserved.
+//
+
+import Foundation
---
+++ CakesHub/CakesHub/TestView.swift
@@ -0,0 +1,20 @@
+//
+//
+//  TestView.swift
+//  CakesHub
+//
+//  Created by Dmitriy Permyakov on 08.05.2024.
+//  Copyright 2024 © VK Team CakesHub. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
